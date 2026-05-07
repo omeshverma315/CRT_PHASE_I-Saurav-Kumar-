d37fcac8a026e7c6a3c46a6d473387a874583f51
omeshverma315 <omeshverma565@gmail.com>
2026-05-07 10:54:03 +0530

    commit

---
+++ Day-7/heirarichal.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+// Base Class
+class Player
+{
+public:
+    string name;
+    int age;
+
+    void setName(string n)
+    {
+        name = n;
+    }
+};
+
+// Derived Class 1
+class Cricketer : public Player
+{
+public:
+    int runs;
+
+    void setRuns(int r)
+    {
+        runs = r;
+    }
+
+    void showCricketer()
+    {
+        cout << "Cricketer Name: " << name << endl;
+        cout << "Runs: " << runs << endl;
+    }
+};
+
+// Derived Class 2
+class Footballer : public Player
+{
+public:
+    int goals;
+
+    void setGoals(int g)
+    {
+        goals = g;
+    }
+
+    void showFootballer()
+    {
+        cout << "Footballer Name: " << name << endl;
+        cout << "Goals: " << goals << endl;
+    }
+};
+
+int main()
+{
+    Cricketer c1;
+    c1.setName("Virat");
+    c1.setRuns(12000);
+    Footballer f1;
+    f1.setName("Messi");
+    f1.setGoals(850);
+
+    cout << "Cricketer Details " << endl;
+    c1.showCricketer();
+    cout << endl;
+    cout << "Footballer Details" << endl;
+    f1.showFootballer();
+
+    return 0;
+}
